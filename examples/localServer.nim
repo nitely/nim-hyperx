99cb68f746b067cf43e15b680cde82743aef8ab7
Esteban C Borsani <ecastroborsani@gmail.com>
2024-09-17 17:53:38 -0300

    Serve API (#27)

--- examples/localServer.nim
+++ examples/localServer.nim
@@ -3,7 +3,6 @@
 {.define: ssl.}
 
 from std/os import getEnv
-import std/strutils
 import std/asyncdispatch
 import ../src/hyperx/server
 
@@ -14,72 +13,30 @@ const keyFile = getEnv "HYPERX_TEST_KEYFILE"
 
 proc processStream(strm: ClientStream) {.async.} =
   ## Full-duplex echo stream
-  with strm:
-    let data = new string
-    await strm.recvHeaders(data)
-    await strm.sendHeaders(
-      @[(":status", "200")], finish = false
-    )
-    if strm.recvEnded:
-      data[] = "Hello world!"
-      await strm.sendBody(data, finish = true)
-    while not strm.recvEnded:
-      data[].setLen 0
-      await strm.recvBody(data)
-      await strm.sendBody(data, finish = strm.recvEnded)
-    if not strm.sendEnded:
-      # recv ended while sending; trailer headers or empty data recv
-      data[].setLen 0
-      await strm.sendBody(data, finish = true)
+  let data = new string
+  await strm.recvHeaders(data)
+  await strm.sendHeaders(
+    @[(":status", "200")], finish = false
+  )
+  if strm.recvEnded:
+    data[] = "Hello world!"
+    await strm.sendBody(data, finish = true)
+  while not strm.recvEnded:
+    data[].setLen 0
+    await strm.recvBody(data)
+    await strm.sendBody(data, finish = strm.recvEnded)
+  if not strm.sendEnded:
+    # recv ended while sending; trailer headers or empty data recv
+    data[].setLen 0
+    await strm.sendBody(data, finish = true)
 
-proc processStreamHandler(strm: ClientStream, propagateErr: bool) {.async.} =
-  try:
-    await processStream(strm)
-  except HyperxStrmError as err:
-    if propagateErr:
-      raise err
-    debugEcho err.msg
-  except HyperxConnError as err:
-    if propagateErr:
-      raise err
-    debugEcho err.msg
-
-proc processClient(client: ClientContext, propagateErr: bool) {.async.} =
-  with client:
-    while client.isConnected:
-      let strm = await client.recvStream()
-      asyncCheck processStreamHandler(strm, propagateErr)
-
-proc processClientHandler(client: ClientContext, propagateErr: bool) {.async.} =
-  try:
-    await processClient(client, propagateErr)
-  except HyperxConnError as err:
-    if propagateErr:
-      raise err
-    debugEcho err.msg
-
-# xxx propagateErr = false
-proc serve*(server: ServerContext, propagateErr = true) {.async.} =
-  with server:
-    while server.isConnected:
-      let client = await server.recvClient()
-      asyncCheck processClientHandler(client, propagateErr)
-
-proc newServer*(): ServerContext =
-  newServer(
+proc main() {.async.} =
+  echo "Serving forever"
+  let server = newServer(
     localHost, localPort, certFile, keyFile
   )
+  await server.serve(processStream)
 
 when isMainModule:
-  proc main() {.async.} =
-    echo "Serving forever"
-    var server = newServer()
-    await server.serve(propagateErr = false)
-  when true:
-    waitFor main()
-  else:  # this is better for profiling/benchmarking but uses 100% CPU
-    var fut = main()
-    while not fut.finished:
-      poll(0)
-    fut.read()
+  waitFor main()
   echo "ok"
